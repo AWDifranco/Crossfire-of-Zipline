4120883a057a5fdf6c2a9fbd99796e0c3e7dea5f
AWDifranco <tony@adtelco.com>
2021-01-30 20:30:13 -0500

    Update __init__.py

--- zipline/__init__.py
+++ zipline/__init__.py
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from distutils.version import StrictVersion
+# Local version in Distribution ARM Cortex 
+from distutils.version import StrictVersion 
 import os
 import numpy as np
 
